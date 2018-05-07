c5ba68b3e63981c4579031de8e92403560f1d0d2
Harsh Thaker <hst.sidhpur@gmail.com>
2018-05-07 15:00:18 +0200

    Create load_data.py

---
+++ load_data.py
@@ -0,0 +1,16 @@
+def load_data(data_dir):
+     
+    directories = [d for d in os.listdir(data_dir) 
+                   if os.path.isdir(os.path.join(data_dir, d))]
+     
+    image_id = []
+    images = []
+    for d in directories:
+        label_dir = os.path.join(data_dir, d)
+        file_names = [os.path.join(label_dir, f) 
+                      for f in os.listdir(label_dir) 
+                      if f.endswith(".jpg")]
+        for f in file_names:
+            images.append(data.imread(f))
+            image_idid.append(file_names)
+    return images, image_id
